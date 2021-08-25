e5d2b6deba81d8fae811013167ad2e93f653b879
Kok Fong <1901887@sit.singaporetech.edu.sg>
2021-08-26 03:06:46 +0800

    did a bunch of things

--- app.py
+++ app.py
@@ -16,20 +16,28 @@ def menu():
 def clean():
     return render_template("clean.html")
 
-@app.route("/updateHum")
+@app.route("/progressbar")
+def pBar():
+    return render_template("progressbar.html")
+
+@app.route("/syringeDetection", methods = ['POST'])
+def syringeDetection():
+    return("test")
+
+@app.route("/updateTempHum")
 def updateHum():
     return jsonify(str(random.randint(0,100)))
 
-@app.route("/updateTemp")
-def updateTemp():
-    return str(random.randint(0,100))
-
-@app.route("/updateTimer")
-def updateTimer():
-    time = 600
-    return jsonify(time)
-
+@app.route('/timerML', methods = ['POST'])
+def timerML():
+    data = request.json
+    temperature = data['temperature']
+    humidity = data['humidity']
+    timePassed = data['timePassed']
+    print(temperature)
+    print(humidity)
+    print(timePassed)
+    return(jsonify(str(random.randint(0,100))))
 
 if __name__ == "__main__":
-    
     app.run(debug=True)
